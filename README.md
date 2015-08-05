e0550fe730f40f0e23a681d66158e5093794518f
Stijn Callebaut <stijnc@fooji.net>
2015-08-05 17:04:51 +0200

    initial  project commit
    added visual studio projects and service manager classes

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# azure-automation-webhook-scsm-connector
-System Center Service Manager connector and runbook template for Azure Automation leveraging webhooks
+# Azure automation webhook Service Manager (SCSM) connector
+System Center Service Manager connector and runbook template for Azure Automation leveraging webhooks.
+
+**Note: credentials are currently stored in clear text!
+
+## overview
+the solution contains the following elements:
+* an Azure Automation Connector
+* an Azure Automation Webhook class
+* an Azure Automation Webhook Activity
+* a Service manager workflow using the azure powershell module to check the runbook job status 
---
+++ SCSM.AzureAutomation/SCSM.AzureAutomation.Worklfow/Start-AAWebhook.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+using System.ComponentModel; 
+using System.ComponentModel.Design; 
+using System.Workflow.ComponentModel.Design; 
+using System.Workflow.ComponentModel; 
+using System.Workflow.ComponentModel.Serialization; 
+using System.Workflow.ComponentModel.Compiler; 
+using System.Collections; 
+using System.Workflow.Activities; 
+using System.Workflow.Runtime; 
+
+
+namespace SCSM.AzureAutomation.Worklfow
+{
+    public partial class Start_AAWebhookActivity : System.Workflow.Activities.SequentialWorkflowActivity
+    {
+        // this is a dummy Workflow service manager needs. All logic is contained in the MP (when using a PowerShell script, otherwise service manager is perfectly capable to handle WWF)
+    }
+}
