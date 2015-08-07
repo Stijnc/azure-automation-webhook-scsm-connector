3e2b70146f516715e3ab95df4a1662fa202d0dc5
Stijn Callebaut <stijnc@fooji.net>
2015-08-07 18:50:43 +0200

    Webhook configuration item
    Relationships
    Form
    View
    Typeprojections
    
    issues:
    icon in form
    listpicker

---
+++ SCSM.AzureAutomation/SCSM.AzureAutomation.WPF/ConfigItem/AzureWebhookForm.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.EnterpriseManagement.UI.DataModel;
+using Microsoft.EnterpriseManagement.UI.FormsInfra;
+using Microsoft.EnterpriseManagement.ServiceManager.Application.Common;
+
+namespace SCSM.AzureAutomation.WPF.ConfigItem
+{
+    /// <summary>
+    /// Interaction logic for AzureWebhookForm.xaml
+    /// </summary>
+    public partial class AzureWebhookForm : UserControl
+    {
+        public AzureWebhookForm()
+        {
+            InitializeComponent();
+
+            RelatedItemsPane relatedItemsPane = new RelatedItemsPane(new ConfigItemRelatedItemsConfiguration());
+            tabItemRelItems.Content = relatedItemsPane;
+        }
+
+        private void ProjectForm_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.AddHandler(FormEvents.PreviewSubmitEvent,
+                new EventHandler<PreviewFormCommandEventArgs>(this.OnPreviewSubmit));
+        }
+
+        private void OnPreviewSubmit(object sender, PreviewFormCommandEventArgs e)
+        {
+            IDataItem itemProject = this.DataContext as IDataItem;
+            string displayName = itemProject["ID"].ToString();
+            itemProject["DisplayName"] = displayName;
+
+        }
+        private void expanderMain_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            HeaderedContentControl headeredContentControl = new HeaderedContentControl
+            {
+                OverridesDefaultStyle = true,
+                Foreground = Brushes.Black,
+                Header = "Azure Webhook Identity"
+            };
+            this.ExpanderMain.Header = headeredContentControl;
+        }
+    }
+}
