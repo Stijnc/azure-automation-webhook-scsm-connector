2cae54e6393c88afba544d0601650c2255070196
Rob Plank <rplank@gmail.com>
2015-10-03 22:29:05 -0500

    Fixed References and updated code behind

--- SCSM.AzureAutomation/SCSM.AzureAutomation.WPF/ConfigItem/AzureRunbookForm.xaml.cs
+++ SCSM.AzureAutomation/SCSM.AzureAutomation.WPF/ConfigItem/AzureRunbookForm.xaml.cs
@@ -12,16 +12,48 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Microsoft.EnterpriseManagement.UI.DataModel;
+using Microsoft.EnterpriseManagement.UI.FormsInfra;
+using Microsoft.EnterpriseManagement.ServiceManager.Application.Common;
+
 namespace SCSM.AzureAutomation.WPF.ConfigItem
 {
     /// <summary>
     /// Interaction logic for Runbook.xaml
     /// </summary>
-    public partial class Runbook : UserControl
+    public partial class AzureRunbookForm : UserControl
     {
-        public Runbook()
+        public AzureRunbookForm()
         {
             InitializeComponent();
+
+            RelatedItemsPane relatedItemsPane = new RelatedItemsPane(new ConfigItemRelatedItemsConfiguration());
+            tabItemRelItems.Content = relatedItemsPane;
+        }
+
+       
+        private void expanderMain_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            HeaderedContentControl headeredContentControl = new HeaderedContentControl
+            {
+                OverridesDefaultStyle = true,
+                Foreground = Brushes.Black,
+                Header = "Azure Runbook Identity"
+            };
+            this.ExpanderMain.Header = headeredContentControl;
+        }
+
+        private void ProjectForm_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.AddHandler(FormEvents.PreviewSubmitEvent,
+                new EventHandler<PreviewFormCommandEventArgs>(this.OnPreviewSubmit));
+        }
+        private void OnPreviewSubmit(object sender, PreviewFormCommandEventArgs e)
+        {
+            IDataItem itemRunbook = this.DataContext as IDataItem;
+            string displayName = itemRunbook["DisplayName"].ToString();
+            itemRunbook["DisplayName"] = displayName;
+
         }
     }
 }
