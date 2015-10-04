a279c7b9513765ad6e9d1030f65f837b5b603a44
Rob Plank <rplank@gmail.com>
2015-10-03 21:21:16 -0500

    Revert "Added Base Powershell scripts"
    This reverts commit f39014e0d780df8bdff88a68498ca55af446c6d8.

--- SCSM.AzureAutomation/PowerShell Scripts/Complete-SMAARunbookActivity.ps1
+++
@@ -1,8 +0,0 @@
-# base script still needs to be update for the workflow to use
-$ID = ""
-$SMClass = Get-SCSMClass -name SCSM.WorkItem.AzureAutomationWebhookActivity$
-$SMObj = Get-SCSMObject -Class $SMClass -Filter "ID -eq $ID"
-$Params = @{
-Status = $Status
-}
-Set-SCSMObject -SMObject $SMObj -propertyhashtable $Params
--- SCSM.AzureAutomation/PowerShell Scripts/Get-AAJobIDStatus.ps1
+++
@@ -1,6 +0,0 @@
-$JobID = ""
-$secpasswd = ConvertTo-SecureString $passwd -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential ($AAAcount, $secpasswd) 
-Add-AzureAccount -credential $cred
-Switch-AzureMode AzureResourceManager
-$Job = Get-AzureAutomationJob -Id $JobID -ResourceGroupName $ResourcegroupName -AutomationAccountName $AutomationAccountName
